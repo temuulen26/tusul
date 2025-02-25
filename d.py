a6dbf773c2215ff05ddb6fd5e7080fdcd0a322e9
temuulen26 <temuulentemm@gmail.com>
2025-02-25 11:41:08 +0800

    new file

---
+++ d.py
@@ -0,0 +1,8 @@
+a = int(input("garaas utga oruulna uu; "))
+b = int(input("daraagiin toog oruulna uu; "))
+if a>b : 
+    print('ehnii too daraagiin toonoos ih')
+if a==b :
+    print('hoyor too hoorondoo tentsuu')
+else :
+    print('ehnii too ni daraagiin toonoos baga')
